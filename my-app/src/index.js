7754dc3f627d563251af7d7b5791ac4c1d43259d
LucaDeCicco <luca14.decicco@yahoo.com>
2022-10-11 12:39:56 +0300

    add footer

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 import MainBg from './MainBg';
+import StickyFooter from './Footer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <MainBg />
-    <App />
+    {/* <App /> */}
+    <StickyFooter />
   </React.StrictMode>
 );
 
