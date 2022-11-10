a5be75b4dfbb9ed4a77ea3a829612eefda465c86
LucaDeCicco <luca14.decicco@yahoo.com>
2022-11-10 12:29:28 +0200

    display name of user in navbar

--- my-app/src/NavBar.js
+++ my-app/src/NavBar.js
@@ -3,9 +3,26 @@ import Logo from "./images/Logo.png";
 import { NavLink } from 'react-router-dom';
 import { Button } from '@mui/material';
 import ProfileDrawer from "./components/ProfileDrawer";
+import {useEffect, useState} from "react";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import LogoutIcon from "@mui/icons-material/Logout";
+import ListItemText from "@mui/material/ListItemText";
+import LoginIcon from "@mui/icons-material/Login";
+import * as React from "react";
 
 
 function NavBar() {
+    const [currentUser, setCurrentUser] = useState(undefined);
+
+
+    useEffect(() => {
+        const user = JSON.parse(localStorage.getItem("user"));
+        if (user) {
+            setCurrentUser(user);
+        }
+    }, []);
     return (
         <>
             <Navbar bg="dark" variant="dark">
@@ -23,6 +40,11 @@ function NavBar() {
                     <NavLink style={{ textDecoration: 'none' }} to="/hotels"><Button variant="text" color={"info"}><b>HOTELS</b></Button></NavLink>
                     </Nav>
                     <ProfileDrawer/>
+                    {currentUser ? (
+                        <b style={{color:"white"}}>{currentUser.username}</b>
+                    ) : (
+                        <a href={"/login"} style={{textDecoration: "none"}}><b style={{color:"white"}}>Login</b></a>
+                    )}
                 </Container>
             </Navbar>
             <br />
