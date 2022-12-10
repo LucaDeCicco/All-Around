acf2cf93f249555de5412390ec66803c0f59cf44
LucaDeCicco <luca14.decicco@yahoo.com>
2022-12-10 02:15:16 +0200

    pagination feature added

--- El-Proyecte-Grande/src/main/java/com/codecool/ElProyecteGrande/controller/ProductController.java
+++ El-Proyecte-Grande/src/main/java/com/codecool/ElProyecteGrande/controller/ProductController.java
@@ -13,6 +13,7 @@ import com.codecool.ElProyecteGrande.service.ProductService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.*;
 
 import java.text.ParseException;
@@ -53,7 +54,6 @@ public class ProductController {
     @PostMapping("/add-circuit")
     @PreAuthorize("hasRole('ADMIN')")
     public String addCircuit(@RequestBody String product) {
-        System.out.println(product);
         ObjectMapper objectMapper = new ObjectMapper();
         CircuitRequest prod;
         try {
@@ -61,8 +61,12 @@ public class ProductController {
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
+//        System.out.println("add-circuit");
+//        System.out.println("countries");
+//        System.out.println(prod.getCountries());
         CircuitProduct circuitProduct = new CircuitProduct(ProductType.CIRCUIT, prod.getDescription(), prod.getPrice(), prod.getImages(), prod.getLocation(),
                 prod.getItinerary(), prod.getRemainingTickets(), prod.getDepartureDate(), prod.getDays(), prod.getCountries());
+        System.out.println(circuitProduct.getCountries());
         productService.save(circuitProduct);
         return "circuit added successfully";
     }
@@ -77,6 +81,27 @@ public class ProductController {
             }
         }
         return circuitProducts;
+    }
+
+    @GetMapping("allMemCircuitProducts/{pageNumber}")
+    @PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
+    public List<Product> getCircuitProducts(@PathVariable String pageNumber) {
+
+        int fromProduct = (Integer.parseInt(pageNumber)-1)*9+1;
+        int toProduct = (Integer.parseInt(pageNumber)*9);
+        List<Product> circuitProducts = new ArrayList<>();
+        for (Product product : productService.findAll()) {
+            if (product.getProductType() == ProductType.CIRCUIT) {
+                circuitProducts.add(product);
+            }
+        }
+        List<Product> result = new ArrayList<>();
+        for (int i = fromProduct-1; i < circuitProducts.size(); i++) {
+            if (i<toProduct){
+                result.add(circuitProducts.get(i));
+            }
+        }
+        return result;
     }
 
     @PostMapping("/add-resort")
@@ -101,6 +126,26 @@ public class ProductController {
         return resortProducts;
     }
 
+    @GetMapping("allMemResortProducts/{pageNumber}")
+    @PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
+    public List<Product> getResortProducts(@PathVariable String pageNumber) {
+        int fromProduct = (Integer.parseInt(pageNumber)-1)*9+1;
+        int toProduct = (Integer.parseInt(pageNumber)*9);
+        List<Product> resortProducts = new ArrayList<>();
+        for (Product product : productService.findAll()) {
+            if (product.getProductType() == ProductType.RESORT) {
+                resortProducts.add(product);
+            }
+        }
+        List<Product> result = new ArrayList<>();
+        for (int i = fromProduct-1; i < resortProducts.size(); i++) {
+            if (i<toProduct){
+                result.add(resortProducts.get(i));
+            }
+        }
+        return result;
+    }
+
     @PostMapping("/addHotelApi")
     @PreAuthorize("hasRole('ADMIN')")
     public String addHotel(@RequestBody HotelRequest product) throws ParseException, JsonProcessingException {
@@ -120,6 +165,26 @@ public class ProductController {
             }
         }
         return hotelProducts;
+    }
+
+    @GetMapping("allMemHotelProducts/{pageNumber}")
+    @PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
+    public List<Product> getHotelProducts(@PathVariable String pageNumber) {
+        int fromProduct = (Integer.parseInt(pageNumber)-1)*9+1;
+        int toProduct = (Integer.parseInt(pageNumber)*9);
+        List<Product> hotelProducts = new ArrayList<>();
+        for (Product product : productService.findAll()) {
+            if (product.getProductType() == ProductType.HOTEL) {
+                hotelProducts.add(product);
+            }
+        }
+        List<Product> result = new ArrayList<>();
+        for (int i = fromProduct-1; i < hotelProducts.size(); i++) {
+            if (i<toProduct){
+                result.add(hotelProducts.get(i));
+            }
+        }
+        return result;
     }
 
     @GetMapping("allMemProducts")
